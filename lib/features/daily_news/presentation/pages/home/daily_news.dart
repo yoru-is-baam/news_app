75c34d95b7e2756cd6c651c4a5b5dfd7c09e923f
yoru-is-baam <hoanghuynhtuankiet69@gmail.com>
2024-10-24 16:38:07 +0700

    first commit

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        'Daily News',
+        style: TextStyle(
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:news_app_clean_architecture/config/theme/app_themes.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/pages/home/daily_news.dart';
+import 'package:news_app_clean_architecture/injection_container.dart';
+
+void main() async {
+  await initializeDependencies();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme(),
+      home: const DailyNews(),
+    );
+  }
+}
